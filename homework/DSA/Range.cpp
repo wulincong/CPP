8e78152f6f0d8ab2d3d347eb8aff670026b06a00
wulincong <2389170337@qq.com>
2020-07-25 22:04:12 +0800

    add homework Range

---
+++ homework/DSA/Range.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+    //printf("%s\n",argv[1]);
+    FILE *fp; 
+    int n,m;
+    int *S;
+    if(fp = fopen(argv[1],"r") == NULL){
+        exit(1);
+    }
+
+
+    
+    
+    return 0;
+}
