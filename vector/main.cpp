0a9969069406cf5615d28375eb46f350f5ffc431
wulincong <wulincong@users.noreply.github.com>
2020-08-22 21:25:15 +0800

    类定义问题

--- vector/main.cpp
+++ vector/main.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "Vector_.h"
 
-Vector<int> v;
+// Vector<int> v;
 using namespace std;
 template class Vector<int>;
 
@@ -21,31 +21,28 @@ void VisitInt(int i){
 
 int main(int argc, char *argv[]){
 
-    if(argc == 2){
-        int capacity = atoi(argv[1]);
-        Vector<int> v = Vector<int>(capacity);
-        int (*Rand)() = RandInt;
+    // if(argc == 2){
+    //     int capacity = atoi(argv[1]);
+    //     Vector<int> v = Vector<int>(capacity);
+    //     int (*Rand)() = RandInt;
+    //     v.Random(Rand);
+    //     void (*visit)(int) = VisitInt;
+    //     v.traverse(visit);
+    //     v.sort();
+    //     printf("\n第二遍\n");
+    //     v.traverse(visit);
+    // }
+
+    int (*Rand)() = RandInt;
+    void (*visit)(int) = VisitInt;   
+    if(argc > 1){
+        int size = atoi(argv[1]);
+        Vector<int> v = Vector<int>(size);
         v.Random(Rand);
-        void (*visit)(int) = VisitInt;
         v.traverse(visit);
         v.sort();
-        printf("\n第二遍\n");
+        printf("\n");
         v.traverse(visit);
+        printf("\n");
     }
-
-    // int (*Rand)() = RandInt;
-    // void (*visit)(int) = VisitInt;   
-    // if(argc > 1){
-    //     int size = atoi(argv[1]);
-    //     v = Vector<int>(size);
-    //     printf("生成向量空间%d\n",size);
-    //     printf("随机生成未知数填满空间\n");
-    //     v.Random(Rand);
-    //     printf("size = %d\n",v.size()); 
-    //     v.traverse(visit);
-    //     printf("\n排序\n");
-    //     v.sort();
-    //     v.traverse(visit);
-    //     printf("\n");
-    // }
 }
